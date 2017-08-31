65140b50fa2b0c7891dfcceee7a80fe1e00beda4
Simone Celia <simon387@hotmail.it>
2017-08-31 18:17:11 +0200

    fine setup

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
+# log4j demo
+
+## init
 + http://www.mkyong.com/maven/how-to-create-a-java-project-with-maven/
 + mkmdir log4jEx
 + mvn archetype:generate -DgroupId=com.celia -DartifactId=Log4jEx -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false
 + mvn eclipse:eclipse
+
+## compiling
++ mvn eclipse:eclipse
++ mvn package
+
+## running
++ java -cp target/Log4jEx-1.0-SNAPSHOT.jar com.celia.App
 
--- src/main/java/com/celia/App.java
+++ src/main/java/com/celia/App.java
@@ -1,13 +1,16 @@
 package com.celia;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.apache.log4j.Logger;
+
+public class App {
+	
+	private static final Logger appLogger = Logger.getLogger(App.class);
+	
+	public static void main(String[] args) {
+		
+		appLogger.info("start of running");
+		System.out.println("end of runnning");
+		
+	}
+
 }
